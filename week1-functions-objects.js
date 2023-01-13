f0ea6dc789102e21aa3c02ae52d9c3b1703c2e90
Gisele Abzalov <gisele.abzalov@gmail.com>
2023-01-13 13:04:08 -0500

    first commit

---
+++ week1-functions-objects.js
@@ -0,0 +1,27 @@
+// Write a function that adds two numbers together 
+
+// Write a function that returns the square of a number
+
+// Create an object called student and add 3 properties with their values.
+
+function sumAdd (x=0, y=0) {
+    let sum = x + y;
+    return sum;
+}
+
+function squareX (x=0) {
+    let square = x * x; // square can be written as **
+    return square;
+}
+
+const student = {
+    firstName: "John",
+    lastName: "Summit",
+    studentMajor: "Finance",
+    studentMinor: "Sociology"
+}
+
+console.log (sumAdd (20,40) );
+
+console.log (squareX (11) );
+
---
+++ week1-variables.js
@@ -0,0 +1,13 @@
+const country = 'Russia'
+const continent = 'Eurasia'
+
+console.log(country)
+console.log(continent)
+
+const beer = 'CoronaIsMexicanBeer'
+console.log(beer)
+
+const number1 = 30
+const number2 = 40
+const sum = number1 + number2
+console.log(sum)
